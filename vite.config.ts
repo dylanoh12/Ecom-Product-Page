ae5e7ce705488d1daa575dcc1f84c9ccd0b22dcf
Dylanoh12 <maroonedsheep@gmail.com>
2025-03-05 14:33:03 +0800

    -m

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/dylanoh12/Ecom-Product-Page.git/'
+  base: '/Ecom-Product-Page/'
 })
